fa3b41425126504ea6d249850d84a70e0fcbc391
Max Base <maxbasecode@gmail.com>
2024-10-13 20:29:13 +0330

    Adding function input type

--- autoload.php
+++ autoload.php
@@ -2,7 +2,7 @@
 
 
 
-function classAutoloader($className): void
+function classAutoloader(string $className): void
 {
     $baseDir = __DIR__.DIRECTORY_SEPARATOR."Controllers".DIRECTORY_SEPARATOR;
 
